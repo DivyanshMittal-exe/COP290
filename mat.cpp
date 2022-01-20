133b7ec66c0bd9f84dcf2f39b930ae9e7b72f5cc
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-01-20 13:42:51 +0530

    Made matrix class

--- mat.cpp
+++ mat.cpp
@@ -5,58 +5,159 @@ using namespace std;
 
 struct sizeMismatch : public std::exception
 {
-	const char * what () const throw ()
+    const char *what() const throw()
     {
-    	return "Matrix size mismatch";
+        return "Matrix size mismatch";
     }
 };
 
-
-struct mat
+class mat
 {
+public:
     int row;
     int col;
-    
     vector<vector<float>> elements;
+
+    mat() {}
+    mat(string filename)
+    {
+        ifstream input_file(filename);
+        int r, c;
+        input_file >> r;
+        input_file >> c;
+        row = r;
+        col = c;
+        vector<vector<float>> elem;
+
+        for (int i = 0; i < r; i++)
+        {
+            vector<float> col;
+            for (int j = 0; j < c; j++)
+            {
+                float val;
+                input_file >> val;
+                col.push_back(val);
+            }
+            elem.push_back(col);
+        }
+        elements = elem;
+    }
+    float getElement(int i, int j)
+    {
+        vector<float> row = elements.at(i);
+        return row.at(j);
+    }
+    mat relu()
+    {
+        mat ret;
+        vector<vector<float>> elem;
+        ret.row = row;
+        ret.col = col;
+
+        for (int r = 0; r < row; r++)
+        {
+            vector<float> col_vec;
+            for (int c = 0; c < col; c++)
+            {
+                float val = getElement(r, c);
+                val = val > 0 ? val : 0;
+                col_vec.push_back(val);
+            }
+            elem.push_back(col_vec);
+        }
+        ret.elements = elem;
+
+        return ret;
+    }
+    mat mat_tanh()
+    {
+        mat ret;
+        vector<vector<float>> elem;
+        ret.row = row;
+        ret.col = col;
+
+        for (int r = 0; r < row; r++)
+        {
+            vector<float> col_vec;
+            for (int c = 0; c < col; c++)
+            {
+                float val = tanh(getElement(r, c));
+                col_vec.push_back(val);
+            }
+            elem.push_back(col_vec);
+        }
+        ret.elements = elem;
+
+        return ret;
+    }
+    mat max_pooling(int stride)
+    {
+        mat ret;
+        vector<vector<float>> elem;
+        ret.row = row / stride;
+        ret.col = col / stride;
+
+        for (int r = 0; r < row; r += stride)
+        {
+            vector<float> col_vec;
+            for (int c = 0; c < col; c += stride)
+            {
+                float val = getElement(r, c);
+                for (int i = 0; i < stride; i++)
+                {
+                    for (int j = 0; j < stride; j++)
+                    {
+                        val = max(val, getElement(r + i, c + j));
+                    }
+                }
+                col_vec.push_back(val);
+            }
+            elem.push_back(col_vec);
+        }
+        ret.elements = elem;
+
+        return ret;
+    }
+
+    mat avg_pooling(mat m1, int stride)
+    {
+        mat ret;
+        vector<vector<float>> elem;
+        ret.row = row / stride;
+        ret.col = col / stride;
+
+        for (int r = 0; r < row; r += stride)
+        {
+            vector<float> col_vec;
+            for (int c = 0; c < col; c += stride)
+            {
+                float val = 0.0f;
+                for (int i = 0; i < stride; i++)
+                {
+                    for (int j = 0; j < stride; j++)
+                    {
+                        val += getElement(r + i, c + j);
+                    }
+                }
+                val /= (stride * stride);
+                col_vec.push_back(val);
+            }
+            elem.push_back(col_vec);
+        }
+        ret.elements = elem;
+
+        return ret;
+    }
 };
 
-float getElement(mat m, int i, int j){
-    
-    vector<float> row = m.elements.at(i);
-    return row.at(j);
-}
-
-mat readMatrix (string filename){
-    ifstream input_file(filename);
-    mat ret;
-    int r,c;
-    input_file >> r;
-    input_file >> c;
-    ret.row = r;
-    ret.col = c;
-    vector<vector<float>> elem;
-    
-    for (int i = 0; i < r; i++)
-    {
-        vector<float> col;
-        for (int j = 0; j < c; j++)
-        {
-            float val;
-            input_file >> val;
-            col.push_back(val);
-        }
-        elem.push_back(col);
-    }
-    ret.elements = elem;
-    return ret;
-}
-
-mat multiply(mat m1,mat m2){
+mat operator*(mat m1, mat m2)
+{
     mat ret;
     vector<vector<float>> elem;
     try
     {
-        if(m1.col!=m2.row){
+        if (m1.col != m2.row)
+        {
             throw sizeMismatch();
         }
         ret.row = m1.row;
@@ -70,7 +171,7 @@ mat multiply(mat m1,mat m2){
                 float val = 0.0f;
                 for (int i = 0; i < m1.col; i++)
                 {
-                    val += getElement(m1,r,i)*getElement(m2,i,c);
+                    val += m1.getElement(r, i) * m2.getElement(i, c);
                 }
                 col.push_back(val);
             }
@@ -78,23 +179,21 @@ mat multiply(mat m1,mat m2){
         }
         ret.elements = elem;
     }
-    catch(sizeMismatch& e){
-        std::cerr << e.what() << '\n';
-    }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
     return ret;
 }
 
-
-mat add(mat m1,mat m2){
+mat operator+(mat m1, mat m2)
+{
     mat ret;
     vector<vector<float>> elem;
     try
     {
-        if(m1.row!=m2.row || m1.col!=m2.col){
+        if (m1.row != m2.row || m1.col != m2.col)
+        {
             throw sizeMismatch();
         }
         ret.row = m1.row;
@@ -105,160 +204,22 @@ mat add(mat m1,mat m2){
             vector<float> col;
             for (int c = 0; c < m1.col; c++)
             {
-                float val =  getElement(m1,r,c) + getElement(m2,r,c);
+                float val = m1.getElement(r, c) + m2.getElement(r, c);
                 col.push_back(val);
             }
             elem.push_back(col);
         }
         ret.elements = elem;
     }
-    catch(sizeMismatch& e){
-        std::cerr << e.what() << '\n';
-    }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
     return ret;
 }
 
-mat add(mat m1,mat m2){
-    mat ret;
-    vector<vector<float>> elem;
-    try
-    {
-        if(m1.row!=m2.row || m1.col!=m2.col){
-            throw sizeMismatch();
-        }
-        ret.row = m1.row;
-        ret.col = m1.col;
-
-        for (int r = 0; r < m1.row; r++)
-        {
-            vector<float> col;
-            for (int c = 0; c < m1.col; c++)
-            {
-                float val =  getElement(m1,r,c) + getElement(m2,r,c);
-                col.push_back(val);
-            }
-            elem.push_back(col);
-        }
-        ret.elements = elem;
-    }
-    catch(sizeMismatch& e){
-        std::cerr << e.what() << '\n';
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    return ret;
-}
-
-
-mat relu(mat m1){
-    mat ret;
-    vector<vector<float>> elem;
-    ret.row = m1.row;
-    ret.col = m1.col;
-
-    for (int r = 0; r < m1.row; r++)
-    {
-        vector<float> col;
-        for (int c = 0; c < m1.col; c++)
-        {
-            float val =  getElement(m1,r,c);
-            val = val > 0 ? val:0;
-            col.push_back(val);
-        }
-        elem.push_back(col);
-    }
-    ret.elements = elem;
-
-    return ret;
-}
-
-mat mat_tanh(mat m1){
-    mat ret;
-    vector<vector<float>> elem;
-    ret.row = m1.row;
-    ret.col = m1.col;
-
-    for (int r = 0; r < m1.row; r++)
-    {
-        vector<float> col;
-        for (int c = 0; c < m1.col; c++)
-        {
-            float val =  tanh(getElement(m1,r,c));
-            col.push_back(val);
-        }
-        elem.push_back(col);
-    }
-    ret.elements = elem;
-
-    return ret;
-}
-
-mat max_pooling(mat m1,int stride){
-    mat ret;
-    vector<vector<float>> elem;
-    ret.row = m1.row/stride;
-    ret.col = m1.col/stride;
-
-    for (int r = 0; r < m1.row; r+=stride)
-    {
-        vector<float> col;
-        for (int c = 0; c < m1.col; c+=stride)
-        {
-            float val = getElement(m1,r,c);
-            for (int i = 0; i < stride; i++)
-            {
-                for (int j = 0; j < stride; j++)
-                {
-                    val = max(val,getElement(m1,r+i,c+j));
-                }
-            }
-            col.push_back(val);
-        }
-        elem.push_back(col);
-    }
-    ret.elements = elem;
-
-    return ret;
-}
-
-mat avg_pooling(mat m1,int stride){
-    mat ret;
-    vector<vector<float>> elem;
-    ret.row = m1.row/stride;
-    ret.col = m1.col/stride;
-
-    for (int r = 0; r < m1.row; r+=stride)
-    {
-        vector<float> col;
-        for (int c = 0; c < m1.col; c+=stride)
-        {
-            float val = 0.0f;
-            for (int i = 0; i < stride; i++)
-            {
-                for (int j = 0; j < stride; j++)
-                {
-                    val += getElement(m1,r+i,c+j);
-                }
-            }
-            val /= (stride*stride);
-            col.push_back(val);
-        }
-        elem.push_back(col);
-    }
-    ret.elements = elem;
-
-    return ret;
-}
-
-int main(){
-
-    
+int main()
+{
 
     return 0;
 }
