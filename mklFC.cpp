0b11e9075caf4a3f90d96d0e74fb640e6dba19eb
Vaibhav Agarwal <vaibhav20325@gmail.com>
2022-02-21 21:05:28 +0530

    minor fixes in oblas and mkl

--- mklFC.cpp
+++ mklFC.cpp
@@ -122,13 +122,13 @@ int main(int argc, char *argv[])
             // No arguments given
             throw "Invalid format. No arguments given. Check out README for valid format.";
         }
-        eelse if (string(argv[1]) == "mkl" && string(argv[2]) == "fullyconnected")
+        else if (string(argv[1]) == "mkl" && string(argv[2]) == "fullyconnected")
         {
-            if (argc != 6 || argc != 7)
+            if (argc != 7)
             {
                 throw "Invalid format. Correct format is as follows - ./yourcode.out mkl fullyconnected inputmatrix.txt weightmatrix.txt biasmatrix.txt outputmatrix.txt";
             }
-            mklFullyConnected(argv[2], argv[3], argv[4], argv[5]);
+            mklFullyConnected(argv[3], argv[4], argv[5], argv[6]);
         }else if (string(argv[1]) == "timer" && argc == 2) {
             timer();
         }else {
