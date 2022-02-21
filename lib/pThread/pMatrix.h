5e0b8288fa99070d00267785c834472a42ab7162
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-02-21 20:57:22 +0530

    Added readme

--- lib/pThread/pMatrix.h
+++ lib/pThread/pMatrix.h
@@ -12,9 +12,6 @@ class pMatrix:public Matrix<float>
 {
 
 public:
-    // int row;
-    // int col;
-    // std::vector<std::vector<float>> elements;
 
     pMatrix():Matrix<float>(){};
     pMatrix(int row,int col):Matrix<float>(row,col){};
@@ -22,11 +19,6 @@ public:
 
     pMatrix transpose() const;
 
-    // float* getElement (int i, int j) const;
-
-    // void print(const std::string &filename);
-    pMatrix operator*(const pMatrix &Matrix_2);
-    pMatrix operator+(const pMatrix &Matrix_2);
 };
     pMatrix fc(const pMatrix &Matrix_1,const pMatrix &Matrix_2,const pMatrix &Matrix_3);
 
