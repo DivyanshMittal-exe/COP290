cd913052d6ae54ec4823e5a1ac3a67b32fc8c348
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-03-05 01:08:21 +0530

    Added basic code

--- makefile
+++ makefile
@@ -7,38 +7,11 @@ all:
 	@echo "Compiling"
 	@make run
 
-run: main.o ./lib/Plain/Matrix.o ./lib/Plain/Vector.o ./lib/pThread/pMatrix.o
-	g++ -O3 -g -pthread main.o lib/Plain/Matrix.o lib/Plain/Vector.o lib/pThread/pMatrix.o -o yourcode.out
-
-main.o: main.cpp
-	@g++ -O3 -c -g main.cpp
-
-Plain/Matrix.o: lib/Plain/Matrix.cpp
-	@g++ -O3 -c -g ./lib/Plain/Matrix.cpp
-
-pThread/pMatrix.o: lib/pThread/pMatrix.cpp
-	@g++ -O3 -c -g -pthread ./lib/pThread/pMatrix.cpp
-
-Plain/Vector.o: lib/Plain/Vector.cpp
-	@g++ -O3 -c -g ./lib/Plain/Vector.cpp
-
-mkl: mklFC.cpp
-	g++ -I /usr/include/mkl mklFC.cpp -o ./yourcode.out -fopenmp -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lpthread -lm -ldl
-
-oblas: oblasFC.cpp
-	g++ -I/opt/OpenBLAS/include -o yourcode.out oblasFC.cpp  -L/opt/OpenBLAS/lib -lopenblas -lpthread -lgfortran
+run: audio.cpp
+	g++ -I /usr/include/mkl audio.cpp -o ./yourcode.out -fopenmp -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lpthread -lm -ldl
 
 debug: clean run
 	sh debugger.sh debug
-
-pdebug: clean run
-	sh debugger.sh pdebug
-
-debug_all:
-	sh debugger.sh all
-
-plot: plot.sh
-	gnuplot plot.sh
 
 clean:
 	@rm -rf *.o *.out lib/Plain/*.o lib/pThread/*.o 
--- utils/fc_tester.py
+++ utils/fc_tester.py
@@ -1,36 +1,40 @@
 import numpy as np
 from random import randint
+import os
+import threading
 
 def mat_to_file(matrix,filename):
     row,col = matrix.shape
     matrix = np.ravel(matrix, order='F')
-    file = open(filename, "w") 
-    file.write(str(col)+"\n")
+    file = open(filename, "w+") 
     file.write(str(row)+"\n")
+    file.write(str(col)+"\n")
     for e in matrix:
         file.write(str(e)+"\n")
     file.close() 
     
-def make_test():
-    # s = 10
-    # e = 100
-    n = 1000
-    # a = randint(s, e)
-    # b = randint(s, e)
-    # c = randint(s, e)
-    a = n
-    b = n
-    c = n
-    m1 = np.random.random((a,b)).astype(np.float32)
-    m2 = np.random.random((b,c)).astype(np.float32)
-    m3 = np.random.random((a,c)).astype(np.float32)
+def make_test(s):
+    a = b = c = s
+    m1 = np.random.random((a,b))
+    m2 = np.random.random((b,c))
+    m3 = np.random.random((a,c))
 
     m_fc = np.add(np.dot(m1,m2) ,m3)
-    base = "testcases/" + str(n)
-    mat_to_file(m1, base + "/inputmatrix.txt")
-    mat_to_file(m2, base + "/weightmatrix.txt")
-    mat_to_file(m3, base + "/biasmatrix.txt")
-    mat_to_file(m_fc, base + "/outputmatrix.txt")
+
+    mat_to_file(m1,str(s) +  "/"+ "inputmatrix.txt")
+    mat_to_file(m2,str(s) +  "/"+ "weightmatrix.txt")
+    mat_to_file(m3,str(s) + "/"+ "biasmatrix.txt")
+    mat_to_file(m_fc,str(s) +"/"+ "outputmatrix.txt")
+    
+    
+    
+
+
+def maker(i):
+        os.makedirs(os.getcwd()+"/" + str(10*i))
+        make_test(10*i)
+
 
 if __name__ == '__main__':
-    make_test()
+    for i in range(5,30):
+        maker(i)
